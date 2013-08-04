f9ccda6780bfabb8b2b18d3de7cd99418d9838cc
KenR <dev@kenpire.com>
2013-08-04 20:40:02 +0100

    Updated ConnectionManager

--- SSISRabbitMQ.RabbitMQConnectionManager/RabbitMQConnectionManager.cs
+++ SSISRabbitMQ.RabbitMQConnectionManager/RabbitMQConnectionManager.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Dts.Runtime;
+using RabbitMQ.Client;
 
 namespace SSISRabbitMQ.RabbitMQConnectionManager
 {
-  public class RabbitMQConnectionManager
+  [DtsConnection(ConnectionType = "SQLCS",
+  DisplayName = "SqlConnectionManager (CS)",
+  Description = "Connection manager for Sql Server 1")]
+  public class RabbitMQConnectionManager : ConnectionManagerBase
   {
+    public string HostName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public int Port { get; set; }
+    public string VirtualHost { get; set; }
+
+    public RabbitMQConnectionManager()
+    {
+      HostName = "localhost";
+      UserName = "guest";
+      Password = "guest";
+      Port = 5672;
+      VirtualHost = "/";
+    }
+
+    private IConnection connection;
+
+    public override Microsoft.SqlServer.Dts.Runtime.DTSExecResult Validate(Microsoft.SqlServer.Dts.Runtime.IDTSInfoEvents infoEvents)
+    {
+      return DTSExecResult.Success;
+    }
+
+    public override object AcquireConnection(object txn)
+    {
+      ConnectionFactory connFactory = new ConnectionFactory()
+      {
+        UserName = UserName,
+        HostName = HostName,
+        Password = Password,
+        Port = Port,
+        VirtualHost = VirtualHost
+      };
+
+      connection = connFactory.CreateConnection();
+
+      return connection;
+    }
+
+    public override void ReleaseConnection(object connection)
+    {
+      ((IConnection)connection).Close();
+    }
   }
 }
